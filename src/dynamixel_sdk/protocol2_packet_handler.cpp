2d863e7527c5473dec7f79ae83d15019a67164e2
Tønnes Nygaard <tonnesfn@ifi.uio.no>
2018-05-23 10:39:33 +0200

    Improvements for servo communication

--- src/dynamixel_sdk/protocol2_packet_handler.cpp
+++ src/dynamixel_sdk/protocol2_packet_handler.cpp
@@ -30,6 +30,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define TXPACKET_MAX_LEN    (4*1024)
 #define RXPACKET_MAX_LEN    (4*1024)
@@ -415,6 +416,7 @@ int Protocol2PacketHandler::rxPacket(PortHandler *port, uint8_t *rxpacket)
         break;
       }
     }
+    usleep(1000);
   }
   port->is_using_ = false;
 
