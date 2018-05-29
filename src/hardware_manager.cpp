c43e841dc60554a41ec7d262be40beab4c627a4d
Tønnes Nygaard <tonnesfn@ifi.uio.no>
2018-05-29 14:29:47 +0200

    Modified and introduced new messages before beta release

--- src/hardware_manager.cpp
+++ src/hardware_manager.cpp
@@ -4,14 +4,13 @@
 #include "ros/console.h"
 
 #include "dyret_common/Pose.h"
-#include "dyret_common/ServoState.h"
-#include "dyret_common/ServoStateArray.h"
-#include "dyret_common/ServoConfigs.h"
+#include "dyret_common/State.h"
+#include "dyret_common/Configuration.h"
 #include "dyret_common/angleConv.h"
-#include "dyret_common/ActuatorStates.h"
-#include "dyret_common/ActuatorCommand.h"
+#include "dyret_common/ActuatorBoardState.h"
+#include "dyret_common/ActuatorBoardCommand.h"
 
-#include "dyret_common/ConfigureServos.h"
+#include "dyret_common/Configure.h"
 
 #include "dynamixel_wrapper.h"
 
@@ -25,7 +24,7 @@ void poseCommandCallback(const dyret_common::Pose::ConstPtr &msg) {
     dynamixel_wrapper::setServoAngles(msg->revolute);
   }
   if (msg->prismatic.size() != 0){
-    dyret_common::ActuatorCommand actuatorCommandMsg;
+    dyret_common::ActuatorBoardCommand actuatorCommandMsg;
 
     actuatorCommandMsg.length.resize((msg->prismatic.size()));
     actuatorCommandMsg.length = msg->prismatic;
@@ -36,40 +35,32 @@ void poseCommandCallback(const dyret_common::Pose::ConstPtr &msg) {
 }
 
 // Received an actuatorBoardState message:
-void actuatorBoardStatesCallback(const dyret_common::ActuatorStates::ConstPtr &msg) {
+void actuatorBoardStatesCallback(const dyret_common::ActuatorBoardState::ConstPtr &msg) {
 
   for (int i = 0; i < msg->position.size(); i++){
-    prismaticPositions[i] = msg->position[i];
+    prismaticPositions[i] = (float) msg->position[i];
   }
 }
 
-bool servoConfigCallback(dyret_common::ConfigureServos::Request  &req,
-                         dyret_common::ConfigureServos::Response &res) {
+bool servoConfigCallback(dyret_common::Configure::Request  &req,
+                         dyret_common::Configure::Response &res) {
 
   std::vector<int> servoIds;
-  for (int i = 0; i < req.configurations.ids.size(); i++) servoIds.push_back(req.configurations.ids[i]);
+  for (int i = 0; i < req.configuration.revolute.ids.size(); i++) servoIds.push_back(req.configuration.revolute.ids[i]);
   std::vector<float> parameters;
-  for (int i = 0; i < req.configurations.parameters.size(); i++) parameters.push_back(req.configurations.parameters[i]);
+  for (int i = 0; i < req.configuration.revolute.parameters.size(); i++) parameters.push_back(req.configuration.revolute.parameters[i]);
 
-  switch (req.configurations.type) {
-    case dyret_common::ServoConfigs::TYPE_DISABLE_LOG:
-      // Disable logging here
-      ROS_ERROR("Disabling servo logging not yet implemented!");
-      break;
-    case dyret_common::ServoConfigs::TYPE_ENABLE_LOG:
-      // Enable logging here
-      ROS_ERROR("Enabling servo logging not yet implemented!");
-      break;
-    case dyret_common::ServoConfigs::TYPE_DISABLE_TORQUE:
+  switch (req.configuration.revolute.type) {
+    case dyret_common::RevoluteConfig::TYPE_DISABLE_TORQUE:
       ROS_ERROR("Setting servo torque not yet implemented!");
       break;
-    case dyret_common::ServoConfigs::TYPE_ENABLE_TORQUE:
+    case dyret_common::RevoluteConfig::TYPE_ENABLE_TORQUE:
       ROS_ERROR("Setting servo torque not yet implemented!");
       break;
-    case dyret_common::ServoConfigs::TYPE_SET_SPEED:
+    case dyret_common::RevoluteConfig::TYPE_SET_SPEED:
       dynamixel_wrapper::setServoSpeeds(servoIds, parameters);
       break;
-    case dyret_common::ServoConfigs::TYPE_SET_PID:
+    case dyret_common::RevoluteConfig::TYPE_SET_PID:
       ROS_ERROR("Setting servo PIDs not yet implemented!");
       break;
     default:
@@ -86,12 +77,12 @@ int main(int argc, char **argv) {
   ros::init(argc, argv, "hardware_manager");
   ros::NodeHandle n;
 
-  ros::Publisher servoStates_pub = n.advertise<dyret_common::ServoStateArray>("/dyret/servoStates", 5);
-  actuatorCommandPub = n.advertise<dyret_common::ActuatorCommand>("/dyret/actuator_board/command", 1);
+  ros::Publisher servoStates_pub = n.advertise<dyret_common::State>("/dyret/state", 5);
+  actuatorCommandPub = n.advertise<dyret_common::ActuatorBoardCommand>("/dyret/actuator_board/command", 1);
 
-  ros::ServiceServer service = n.advertiseService("/dyret/configure_servos", servoConfigCallback);
-  ros::Subscriber poseCommand_sub = n.subscribe("/dyret/pose_command", 1, poseCommandCallback);
-  ros::Subscriber actuatorBoardStates_sub = n.subscribe("/dyret/actuator_board/states", 1, actuatorBoardStatesCallback);
+  ros::ServiceServer service = n.advertiseService("/dyret/configuration", servoConfigCallback);
+  ros::Subscriber poseCommand_sub = n.subscribe("/dyret/command", 1, poseCommandCallback);
+  ros::Subscriber actuatorBoardStates_sub = n.subscribe("/dyret/actuator_board/state", 1, actuatorBoardStatesCallback);
 
   std::vector<int> servoIds = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
@@ -108,15 +99,18 @@ int main(int argc, char **argv) {
 
   while (ros::ok()) {
 
-    dyret_common::ServoStateArray servoStates;
+    dyret_common::State servoStates;
 
     std::vector<float> servoAngles = dynamixel_wrapper::getServoAngles(servoIds);
 
-    for (int i = 0; i < servoStates.revolute.size(); i++) {
+    for (int i = 0; i < servoAngles.size(); i++) {
       servoStates.revolute[i].position = servoAngles[i];
     }
 
-    servoStates.prismatic = prismaticPositions;
+    for (int i = 0; i < prismaticPositions.size(); i++) {
+      servoStates.prismatic[i].position = prismaticPositions[i];
+    }
+
 
     servoStates_pub.publish(servoStates);
 
